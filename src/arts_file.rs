ff9adc1c214daa673420d1b78d7b3fe20aaf270d
Justiks1337 <vkusniycat@yandex.ru>
2024-06-21 00:48:13 +0400

    fix work with files

--- src/arts_file.rs
+++ src/arts_file.rs
@@ -1,41 +1,39 @@
 pub mod arts_file {
-    use std::fs::File;
-    use std::io::{BufRead, BufReader, Lines};
+    use std::io::{BufRead, BufReader};
     use crate::art::arts::Art;
 
-    pub struct ArtsFile {
-        file: File,
+    pub struct ArtsFile <'a> {
+        file: &'a str,
         pub arts: Vec<Art>,
         pub arts_amount: usize,
     }
 
-    impl ArtsFile {
+    impl ArtsFile<'_> {
         pub fn new() -> Self {
-            let file = File::open("arts.txt");
+            let file = include_str!("../arts.txt");
             let arts: Vec<Art> = Vec::new();
             let arts_amount: usize = 0;
 
-            ArtsFile { file: file.expect("REASON"), arts, arts_amount}
+            ArtsFile { file: file, arts, arts_amount}
         }
 
         pub fn read_file(&mut self) {
-            let file_clone = self.file.try_clone().expect("Failed load arts.txt");
-            let reader = BufReader::new(file_clone);
-            self.read_art(reader.lines());
+            let file_clone = self.file.clone();
+            self.read_art(file_clone.lines());
         }
 
-        fn read_art(&mut self, lines: Lines<BufReader<File>>) {
+        fn read_art(&mut self, lines: core::str::Lines) {
             let mut art = Art::new();
             let mut index: usize = 0;
             for line_result in lines {
-                if let Ok(line) = line_result {
+                if let line = line_result {
                     if line.starts_with('-') {
                         self.arts.push(art);
                         index = 0;
                         self.arts_amount += 1;
                         art = Art::new()
                     } else {
-                        art.add_line(line, index);
+                        art.add_line(line.to_string(), index);
                         index += 1;
                     }
                 }
